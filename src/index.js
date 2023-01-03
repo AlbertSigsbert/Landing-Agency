a318e4c500fae4fa9eb7c1840817bf1573bda22f
Albert <ishengomaalbert@gmail.com>
2023-01-03 11:39:59 +0300

    Landing Page for Digital Agency

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var menuBtn = document.getElementById('openBtn')
+var closeMenu = document.getElementById('closeBtn')
+
+menuBtn.addEventListener('click ', (e) => {
+    e.preventDefault()
+    console.log('123');
+})
